295cd8b4deafc3ad104ad34a814550a800eb6f17
nickozoulis <nikolaos-stylianos.zoulis@sky.uk>
2017-04-25 09:30:36 +0100

    drag stuff, alchemist, elixir, flycheck

--- init.el
+++ init.el
@@ -38,6 +38,20 @@
 ;; PACKAGES
 ;;
 
+(global-set-key (kbd "C-n")
+    (lambda () (interactive) (next-line 5)))
+
+(global-set-key (kbd "C-p")
+    (lambda () (interactive) (previous-line 5)))
+
+(use-package drag-stuff
+  :ensure t
+  :diminish drag-stuff-mode
+  :bind (("M-<down>" . drag-stuff-down)
+         ("M-<up>" . drag-stuff-up))
+  :config
+  (drag-stuff-global-mode))
+
 (use-package magit
   :bind (("C-x g" . magit-status))
   :ensure t)
@@ -52,7 +66,21 @@
 (use-package neotree
   :ensure t)
 
+
+(use-package exec-path-from-shell
+  :ensure t
+  :init (progn
+	  (when (memq window-system '(mac ns x))
+	    (exec-path-from-shell-initialize))
+	  ))
+
 (setq neo-smart-open t)
+
+(use-package flycheck
+  :ensure t
+  :init (progn
+	  (global-flycheck-mode t)
+	  ))
 
 (use-package erlang
   :ensure t
@@ -66,9 +94,51 @@
          ("\\.app\\(\\.src\\)?\\'" . erlang-mode))
   :config
   (setq erlang-indent-level 4))
+
+;; (use-package elixir-mode
+;;   :mode ("\\.ex\\'" "\\.exs\\'" "mix\\.lock\\'")
+;;   :config)
+
+(use-package alchemist
+  :ensure t
+  :config (progn
+	    ;; (setq alchemist-goto-erlang-source-dir "~/projects/open-source/otp/")
+	    (setq alchemist-goto-elixir-source-dir "/Users/nzo01/emacs_libs/elixir/")    
+	    ;; (setq alchemist-key-command-prefix (kbd "C-c ,")) ;; default: (kbd "C-c a")
+	    (setq alchemist-test-display-compilation-output t)
+	    (setq alchemist-hooks-test-on-save nil)
+	    (setq alchemist-hooks-compile-on-save nil)
+
+	    ;; allows the jumping back out of erlang code
+	    (defun custom-erlang-mode-hook ()
+	      (define-key erlang-mode-map (kbd "M-,") 'alchemist-goto-jump-back))
+	    (add-hook 'erlang-mode-hook 'custom-erlang-mode-hook)
+	    ))
+
+(use-package flycheck-mix
+  :ensure t
+  :config (progn
+	    (flycheck-mix-setup)
+	    ))
+
+
+(use-package flycheck-mix
+  :ensure t)
+
+(use-package flycheck-elixir
+  :ensure t)
+
+
 (use-package elixir-mode
-  :mode ("\\.ex\\'" "\\.exs\\'" "mix\\.lock\\'")
-  :config)
+  :ensure t
+  :init (progn
+	  (add-hook 'elixir-mode-hook 'company-mode)
+	  (add-hook 'elixir-mode-hook 'alchemist-mode)
+	  (add-hook 'elixir-mode-hook 'elixir-add-electric-pairs)
+	  (add-hook 'elixir-mode-hook 'flycheck-elixir)
+	  (add-hook 'elixir-mode-hook 'flycheck-mode)
+	  )
+  )
 
 (use-package wh-smarter-beginning-of-line
   :bind ("C-a" . wh/smarter-beginning-of-line))
@@ -76,6 +146,34 @@
 ;;
 ;; VISUAL CHANGES
 ;;
+
+;; make the fringe stand out from the background
+(setq solarized-distinct-fringe-background t)
+
+;; Don't change the font for some headings and titles
+(setq solarized-use-variable-pitch nil)
+
+;; make the modeline high contrast
+(setq solarized-high-contrast-mode-line t)
+
+;; Use less bolding
+(setq solarized-use-less-bold t)
+
+;; Use more italics
+(setq solarized-use-more-italic t)
+
+;; Use less colors for indicators such as git:gutter, flycheck and similar
+(setq solarized-emphasize-indicators nil)
+
+;; Don't change size of org-mode headlines (but keep other size-changes)
+(setq solarized-scale-org-headlines nil)
+
+;; Avoid all font-size changes
+(setq solarized-height-minus-1 1.0)
+(setq solarized-height-plus-1 1.0)
+(setq solarized-height-plus-2 1.0)
+(setq solarized-height-plus-3 1.0)
+(setq solarized-height-plus-4 1.0)
 
 ;; Always as "y or n", not that annoying "yes or no".
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -118,10 +216,10 @@
 ;; limit the number of times a frame can split
 (setq split-width-threshold 200)
 
-(set-frame-font "Inconsolata 13" t t)
+(set-frame-font "Inconsolata 15" t t)
 
 ;; for new frames and emacs client..
-(setq default-frame-alist '((font . "Inconsolata 13")))
+(setq default-frame-alist '((font . "Inconsolata 15")))
 
 ;; set font size
 (set-face-attribute 'default nil :height 120)
@@ -141,7 +239,9 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
- '(package-selected-packages (quote (solarized-theme use-package))))
+ '(package-selected-packages
+   (quote
+    (drag-stuff exec-path-from-shell flycheck-elixir flycheck-mix flycheck alchemist solarized-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
